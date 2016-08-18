ced682cfc4d53295d6259003709b77f52caee82e
dgowens2 <dgowens2@gmail.com>
2016-08-18 11:50:47 -0400

    Still committing

--- src/tiyinc/noobs/Day9Runner.java
+++ src/tiyinc/noobs/Day9Runner.java
@@ -1,5 +1,9 @@
 package tiyinc.noobs;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
  * Created by DTG2 on 08/18/16.
  */
@@ -9,11 +13,52 @@ public class Day9Runner {
         Day9Runner myRunner = new Day9Runner();
         //creates a new instance of the Day9Runner so that we can call non-static methods
         myRunner.testWriteFile();
-
+        myRunner.testReadFile();
     }
-    
+
+    public void testReadFile() {
+        try {
+            File testFile = new File("src/tiyinc/noobs/Day9Runner.java");
+            Scanner fileScanner = new Scanner(testFile);
+            int numLines = 0;
+            int numChars = 0;
+            int numInts = 0;
+            while (fileScanner.hasNext()) {
+                String currentLine = fileScanner.nextLine();
+                numLines++;
+                numChars += currentLine.length();
+                int intIndex = currentLine.indexOf("int");
+                if (intIndex != -1) {
+                    numInts++;
+                }
+                System.out.println(currentLine);
+            }
+            System.out.println("You have " + numChars + " characters in your file!");
+            System.out.println("You have " + numLines + " lines in your file");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     public void testWriteFile() {
         System.out.println("Test write file");
+        FileWriter testWriter = null;
 
+        try {
+            File testFile = new File("noobs.txt");
+            testWriter = new FileWriter(testFile);
+            testWriter.write("Java rocks! Harder!!!");
+//            testWriter.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            if (testWriter != null); {
+                try {
+                    testWriter.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
